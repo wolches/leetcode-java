d8571a5f5ae4e4fb2f98848bdc5a6489802a74c0
wolches <anton.wolches@gmail.com>
2024-08-29 22:27:44 +0300

    solution: longest valid parentheses

---
+++ src/main/java/io/github/wolches/leetcode/solution/longest_valid_parentheses/Solution.java
@@ -0,0 +1,33 @@
+package io.github.wolches.leetcode.solution.longest_valid_parentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+
+    public int longestValidParentheses(String s) {
+        int result = 0;
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '(') {
+                indices.add(i);
+            } else {
+                if (!indices.isEmpty() && s.charAt(indices.getLast()) == '(') {
+                    indices.removeLast();  // valid pair
+                } else {
+                    indices.add(i);
+                }
+            }
+        }
+        if (indices.isEmpty()) {
+            return s.length();
+        }
+        result = indices.get(0);
+        for (int i = 1; i < indices.size(); i++) {
+            result = Math.max(result, indices.get(i) - indices.get(i - 1) - 1);
+        }
+        result = Math.max(result, s.length() - indices.getLast() - 1);
+        return result;
+    }
+}
